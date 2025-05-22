f4deb94b0fe557d37902a23839fc10628a67f184
DelhiDigital <delhidigitalco@gmail.com>
2025-05-22 09:44:45 +0530

    new Final Updated commit 1

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import "./process.css"
+import "./Process.css"
 
 export default function Process() {
   const [activeStep, setActiveStep] = useState(0)
